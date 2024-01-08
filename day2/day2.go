23646bf75b67934657827cc4453d0d4d1f9849fd
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2024-01-08 12:34:53 +0200

    day 2 part 2 solution

--- day2/day2.go
+++ day2/day2.go
@@ -3,14 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/exp/slices"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-	"golang.org/x/exp/slices"
 )
 
-func main() {
+func part1() {
 	file, err := os.Open("./input")
 	if err != nil {
 		log.Fatal(err)
@@ -63,10 +63,68 @@ func main() {
 		validGames = nil
 
 	}
-    
-    for i := 0; i < len(validIds); i ++ {
-        sumIds += validIds[i]
-    }
-    
+
+	for i := 0; i < len(validIds); i++ {
+		sumIds += validIds[i]
+	}
+
 	fmt.Print(sumIds)
+
+}
+
+func part2() {
+
+	file, err := os.Open("./input2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	sumOfProuct := 0
+
+	for scanner.Scan() {
+		games := strings.Split(scanner.Text(), ":")
+
+		rounds := games[1]
+		turn := strings.Split(rounds, ",")
+
+		var maxRed, maxGreen, maxBlue int64
+
+		for _, v := range turn {
+			play := strings.Split(v, " ")
+			fmt.Printf("play: %v\n", play)
+			for i := 0; i < len(play); i++ {
+				num, err := strconv.ParseInt(play[i], 10, 64)
+				if err != nil {
+					fmt.Printf("Non-numeric value: %v\n", play[i])
+					continue
+				}
+				if (play[i+1] == "red" || play[i+1] == "red;") && num > maxRed {
+					maxRed = num
+				}
+
+				if (play[i+1] == "green" || play[i+1] == "green;") && num > maxGreen {
+					maxGreen = num
+				}
+				if (play[i+1] == "blue" || play[i+1] == "blue;") && num > maxBlue {
+					maxBlue = num
+				}
+
+			}
+
+		}
+		product := maxRed * maxGreen * maxBlue
+		sumOfProuct += int(product)
+		product = 0
+
+	}
+
+	fmt.Println(sumOfProuct)
+
+}
+
+func main() {
+	part2()
 }
