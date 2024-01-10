dac7ff8f5e0ae31013e964f7f8e6ac07b46bb477
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2024-01-10 17:59:30 +0200

    day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -1,63 +1,112 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
-	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
-func main() {
+type Data struct {
+	Lines []string
+}
 
-	file, err := os.Open("./input")
+func parse(fileName string) (*Data, error) {
+	buf, err := os.ReadFile(fileName)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("read input file: %w", err)
 	}
-	defer file.Close()
+	d := &Data{}
+	lines := strings.Split(string(buf), "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		d.Lines = append(d.Lines, line)
+	}
+	return d, nil
+}
 
-	scanner := bufio.NewScanner(file)
+func run(_ context.Context) error {
+	d, err := parse("./input")
+	if err != nil {
+		return fmt.Errorf("parse: %w", err)
+	}
 
-	var numbers []string
-	var number string
-	answer := 0
+	part1Tokens := map[string]int{
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+		"5": 5,
+		"6": 6,
+		"7": 7,
+		"8": 8,
+		"9": 9,
+	}
+	_ = part1Tokens
+	part2Tokens := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
+	_ = part2Tokens
 
-	for scanner.Scan() {
-		for _, v := range strings.Split(scanner.Text(), "") {
-			if _, err := strconv.Atoi(v); err == nil {
-				number += v
+	tokens := part2Tokens
+
+	total := 0
+	for _, line := range d.Lines {
+		var left, right int
+	subloop:
+		for i := 0; i < len(line); i++ {
+			for tok, n := range tokens {
+				if i+len(tok) > len(line) {
+					continue
+				}
+				if line[i:i+len(tok)] == tok {
+					left = n
+					break subloop
+				}
 			}
-
-		}
-		if len(number) > 2 {
-			firstChar := string(number[0])
-			lastChar := string(number[len(number)-1])
-			number = firstChar + lastChar
-
 		}
 
-		if len(number) == 1 {
-			number += number
+	subloop2:
+		for i := len(line) - 1; i >= 0; i-- {
+			for tok, n := range tokens {
+				if i-len(tok)+1 < 0 {
+					continue
+				}
+				if line[i-len(tok)+1:i+1] == tok {
+					right = n
+					break subloop2
+				}
+			}
 		}
-		numbers = append(numbers, number)
-		number = ""
+		total += left*10 + right
 	}
+	fmt.Printf("%d\n", total)
+	return nil
+}
 
-
-	for _, v := range numbers {
-		i, err := strconv.Atoi(v)
-    
-        answer += i
-		if err != nil {
-			panic(err)
-		}
-
+func main() {
+	if err := run(context.Background()); err != nil {
+		println("Fail:", err.Error())
+		return
 	}
-
-    fmt.Println(answer)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	println("success")
 }
